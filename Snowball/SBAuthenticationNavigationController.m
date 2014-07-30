b5d83768dd81dd1a5a3f46de28640ed4df53f5f4
James Martinez <jamescmartinez@gmail.com>
2014-07-30 14:05:03 -0700

    auth nc dismiss in main queue

--- Snowball/SBAuthenticationNavigationController.m
+++ Snowball/SBAuthenticationNavigationController.m
@@ -15,20 +15,22 @@
 @implementation SBAuthenticationNavigationController
 
 - (void)dismiss {
-    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    UIViewController *vc = [storyboard instantiateInitialViewController];
-    [UIView transitionWithView:[UIApplication sharedApplication].delegate.window
-                      duration:.8
-                       options:UIViewAnimationOptionTransitionFlipFromLeft
-                    animations:^{
-                        // Without disabling and reenabling animations, some funky things happen.
-                        // http://stackoverflow.com/questions/8053832/rootviewcontroller-animation-transition-initial-orientation-is-wrong
-                        BOOL oldState = [UIView areAnimationsEnabled];
-                        [UIView setAnimationsEnabled:NO];
-                        [[UIApplication sharedApplication].delegate.window setRootViewController:vc];
-                        [UIView setAnimationsEnabled:oldState];
-                    }
-                    completion:nil];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        UIViewController *vc = [storyboard instantiateInitialViewController];
+        [UIView transitionWithView:[UIApplication sharedApplication].delegate.window
+                          duration:.8
+                           options:UIViewAnimationOptionTransitionFlipFromLeft
+                        animations:^{
+                            // Without disabling and reenabling animations, some funky things happen.
+                            // http://stackoverflow.com/questions/8053832/rootviewcontroller-animation-transition-initial-orientation-is-wrong
+                            BOOL oldState = [UIView areAnimationsEnabled];
+                            [UIView setAnimationsEnabled:NO];
+                            [[UIApplication sharedApplication].delegate.window setRootViewController:vc];
+                            [UIView setAnimationsEnabled:oldState];
+                        }
+                        completion:nil];
+    });
 }
 
 @end
