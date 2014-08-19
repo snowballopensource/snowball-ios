edd56870f2d0d57a8addd1beaf5f98e6c4084853
James Martinez <jamescmartinez@gmail.com>
2014-08-18 18:35:15 -0700

    delete store on model mismatch

--- Snowball/SBAppDelegate.m
+++ Snowball/SBAppDelegate.m
@@ -14,6 +14,7 @@
 @implementation SBAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [MagicalRecord setShouldDeleteStoreOnModelMismatch:YES];
     [MagicalRecord setupAutoMigratingCoreDataStack];
     [SBSessionManager startSession];
     [SBPushNotificationManager setup];
