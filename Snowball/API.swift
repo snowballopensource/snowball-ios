f2d82dead1375d3134799228c047a43b31a74da5
James Martinez <jamescmartinez@gmail.com>
2014-09-18 03:07:29 -0700

    laid out user api signatures

---
+++ Snowball/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  Snowball
+//
+//  Created by James Martinez on 9/18/14.
+//  Copyright (c) 2014 Snowball, Inc. All rights reserved.
+//
+
+import Foundation
+
+typealias successClosure = () -> ()
+typealias failureClosure = (NSError) -> ()
--- Snowball/User.swift
+++ Snowball/User.swift
@@ -9,13 +9,46 @@
 import Foundation
 
 class User: RLMObject {
-
   dynamic var name = ""
   dynamic var username = ""
   dynamic var avatarURL = ""
   dynamic var email = ""
   dynamic var phoneNumber = ""
+}
 
-  class func signUp(#username: String, email: String, password: String) {
+class UserAPI {
+  class func signUp(#username: String, email: String, password: String, success: successClosure?, failure: failureClosure?) {
+
   }
+
+  class func signIn(#email: String, password: String, success: successClosure?, failure: failureClosure?) {
+
+  }
+
+  class func getCurrentUser(success: successClosure?, failure: failureClosure?) {
+
+  }
+
+  class func updateCurrentUser(success: successClosure?, failure: failureClosure?) {
+
+  }
+
+  class func getCurrentUserFollowing(success: successClosure?, failure: failureClosure?) {
+
+  }
+
+  class func followUser(user: User, success: successClosure?, failure: failureClosure?) {
+
+  }
+
+  class func unfollowUser(user: User, success: successClosure?, failure: failureClosure?) {
+
+  }
+
+  typealias successClosureWithUsers = ([User]?) -> ()
+  class func findUsersByPhoneNumbers(phoneNumbers: [String], success: successClosureWithUsers?, failure: failureClosure?) {
+    let users = [User]()
+    if (success != nil) { success!(users) }
+  }
+
 }
