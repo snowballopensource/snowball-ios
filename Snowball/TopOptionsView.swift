33db165eb2469814effab71f77fec48527eb96b5
James Martinez <jamescmartinez@gmail.com>
2014-09-23 18:12:19 -0700

    add subviews to top options view

--- Snowball/TopOptionsView.swift
+++ Snowball/TopOptionsView.swift
@@ -6,15 +6,32 @@
 //  Copyright (c) 2014 Snowball, Inc. All rights reserved.
 //
 
+import Cartography
 import UIKit
 
 class TopOptionsView: UIView {
+  private let cameraView = UIView()
+  private let addButton = UIButton()
+  private let videoPreviewView = UIView()
+  private var showPreviewView = false
+
+  func animate() {
+    showPreviewView = !showPreviewView
+    // TODO: animate
+  }
 
   // MARK: UIView
 
   override init(frame: CGRect) {
     super.init(frame: frame)
-    backgroundColor = UIColor.lightGrayColor()
+    cameraView.backgroundColor = UIColor.lightGrayColor()
+    addSubview(cameraView)
+    addButton.backgroundColor = UIColor.darkGrayColor()
+    addSubview(addButton)
+    videoPreviewView.backgroundColor = UIColor.lightGrayColor()
+    addSubview(videoPreviewView)
+
+    addButton.addTarget(self, action: Selector("animate"), forControlEvents: UIControlEvents.TouchUpInside)
   }
 
   required init(coder aDecoder: NSCoder) {
@@ -23,6 +40,27 @@ class TopOptionsView: UIView {
 
   convenience override init() {
     self.init(frame: CGRectZero)
+  }
+
+  override func layoutSubviews() {
+    layout(cameraView) { (cameraView) in
+      cameraView.top == cameraView.superview!.top
+      cameraView.bottom == cameraView.superview!.bottom
+      cameraView.left == cameraView.superview!.left
+      cameraView.width == cameraView.superview!.width / 2
+    }
+    layout(addButton, cameraView) { (addButton, cameraView) in
+      addButton.top == addButton.superview!.top
+      addButton.bottom == addButton.superview!.bottom
+      addButton.left == cameraView.right
+      addButton.width == addButton.superview!.width / 2
+    }
+    layout(videoPreviewView, addButton) { (videoPreviewView, addButton) in
+      videoPreviewView.top == videoPreviewView.superview!.top
+      videoPreviewView.bottom == videoPreviewView.superview!.bottom
+      videoPreviewView.left == addButton.right
+      videoPreviewView.width == videoPreviewView.superview!.width / 2
+    }
   }
 
 }
