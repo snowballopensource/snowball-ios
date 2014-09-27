a000d6c13ace7bef6a930d6947891edf169259c4
James Martinez <jamescmartinez@gmail.com>
2014-09-27 13:07:21 -0700

    removed thumbnails from clip

--- Snowball/Clip.swift
+++ Snowball/Clip.swift
@@ -10,7 +10,6 @@ import Foundation
 
 class Clip: RLMObject {
   dynamic var id = ""
-  dynamic var thumbnailURL = ""
   dynamic var videoURL = ""
   dynamic var createdAt = NSDate(timeIntervalSince1970: 0)
 
@@ -22,9 +21,6 @@ class Clip: RLMObject {
   override func updateFromDictionary(dictionary: [String: AnyObject]) {
     if let id = dictionary["id"] as AnyObject? as? String {
       self.id = id
-    }
-    if let thumbnailURL = dictionary["thumbnail_url"] as AnyObject? as? String {
-      self.thumbnailURL = thumbnailURL
     }
     if let videoURL = dictionary["video_url"] as AnyObject? as? String {
       self.videoURL = videoURL
