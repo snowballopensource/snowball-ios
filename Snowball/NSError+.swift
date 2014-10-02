016722e7d60954631a00b13212b32160a2c48657
James Martinez <jamescmartinez@gmail.com>
2014-10-01 22:28:12 -0700

    fixed error alert controller presentation

--- Snowball/NSError+.swift
+++ Snowball/NSError+.swift
@@ -11,6 +11,9 @@ import UIKit
 
 extension NSError {
   func display() {
-    UIAlertController(title: NSLocalizedString("Error"), message: localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+    let rootViewController = UIApplication.sharedApplication().delegate!.window!!.rootViewController
+    let alertController = UIAlertController(title: NSLocalizedString("Error"), message: localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+    alertController.addAction(UIAlertAction(title: NSLocalizedString("OK"), style: UIAlertActionStyle.Cancel, handler: nil))
+    rootViewController?.presentViewController(alertController, animated: true) {}
   }
 }
