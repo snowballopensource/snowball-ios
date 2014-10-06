c0efd81425e1a7b194889a6551c65dfbf114c59d
James Martinez <jamescmartinez@gmail.com>
2014-10-06 11:23:22 -0700

    blue navigation bar color

--- Snowball/AppDelegate.swift
+++ Snowball/AppDelegate.swift
@@ -21,12 +21,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
   }
 
-  // MARK: - UIApplicationDelegate
+  private func setAppearance() {
+    // UINavigationBar
+    // http://stackoverflow.com/a/18969325/801858
+    UINavigationBar.appearance().setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+    UINavigationBar.appearance().shadowImage = UIImage()
+    UINavigationBar.appearance().translucent = false
+    UINavigationBar.appearance().barTintColor = UIColor.SnowballColor.blue()
+  }
+
+  // MARK: UIApplicationDelegate
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     window = UIWindow(frame: UIScreen.mainScreen().bounds)
     window?.rootViewController = initialViewController()
     window?.makeKeyAndVisible()
+    setAppearance()
     return true
   }
 
