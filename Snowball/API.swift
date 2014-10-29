5297f041b50d5311b8e54b37915d8d2070142ce1
James Martinez <jamescmartinez@gmail.com>
2014-10-29 09:39:11 -0700

    new api endpoints

--- Snowball/API.swift
+++ Snowball/API.swift
@@ -43,133 +43,91 @@ enum APIRoute: URLRequestConvertible {
   static let baseURLString = "http://snowball-staging.herokuapp.com/api/v1/"
 
   // Authentication
-  case SignUp(username: String, email: String, password: String)
-  case SignIn(email: String, password: String)
+  case PhoneAuthentication(phoneNumber: String)
+  case PhoneVerification(userID: String, phoneNumberVerificationCode: String)
   // User
   case GetCurrentUser
-  case UpdateCurrentUser(username: String?, email: String?, name: String?)
-  case ChangePhoneNumber(phoneNumber: String)
-  case VerifyPhoneNumber(phoneNumberVerificationCode: String)
+  case UpdateCurrentUser(name: String?, username: String?, email: String?)
   case GetCurrentUserFollowing
   case FollowUser(userID: String)
   case UnfollowUser(userID: String)
   case FindUsersByPhoneNumbers(phoneNumbers: [String])
-  // Reel
-  case GetReelStream
-  case CreateReel(title: String?, participantIDs: [String]?)
-  case UpdateReelTitle(reelID: String, title: String)
-  case GetReelParticipants(reelID: String)
-  case AddParticipantToReel(reelID: String, userID: String)
-  case RemoveCurrentUserAsParticipantInReel(reelID: String)
+  case FindUsersByUsername(username: String)
   // Clip
-  case GetUnwatchedClipsInReel(reelID: String, since: NSDate?)
-  case CreateClipInReel(reelID: String, videoData: NSData)
+  case GetClipFeed
+  case CreateClip(videoData: NSData)
+  case DeleteClip(clipID: String)
+  case FlagClip(clipID: String)
 
   var method: Alamofire.Method {
     switch self {
-    case .SignUp: return .POST
-    case .SignIn: return .POST
-    case .GetCurrentUser: return .GET
-    case .UpdateCurrentUser: return .PATCH
-    case .ChangePhoneNumber: return .POST
-    case .VerifyPhoneNumber: return .POST
-    case .GetCurrentUserFollowing: return .GET
-    case .FollowUser: return .POST
-    case .UnfollowUser: return .DELETE
-    case .FindUsersByPhoneNumbers: return .POST
-    case .GetReelStream: return .GET
-    case .CreateReel: return .POST
-    case .UpdateReelTitle: return .PATCH
-    case .GetReelParticipants: return .GET
-    case .AddParticipantToReel: return .POST
-    case .RemoveCurrentUserAsParticipantInReel: return .DELETE
-    case .GetUnwatchedClipsInReel: return .GET
-    case .CreateClipInReel: return .POST
-      }
+      case .PhoneAuthentication: return .POST
+      case .PhoneVerification: return .POST
+      case .GetCurrentUser: return .GET
+      case .UpdateCurrentUser: return .PATCH
+      case .GetCurrentUserFollowing: return .GET
+      case .FollowUser: return .POST
+      case .UnfollowUser: return .DELETE
+      case .FindUsersByPhoneNumbers: return .GET
+      case .FindUsersByUsername: return .GET
+      case .GetClipFeed: return .GET
+      case .CreateClip: return .POST
+      case .DeleteClip: return .DELETE
+      case .FlagClip: return .POST
+    }
   }
 
   var path: String {
     switch self {
-    case .SignUp: return "users/sign_up"
-    case .SignIn: return "users/sign_in"
-    case .GetCurrentUser: return "users/me"
-    case .UpdateCurrentUser: return "users/me"
-    case .ChangePhoneNumber: return "users/me/phone_number_change"
-    case .VerifyPhoneNumber: return "users/me/phone_number_verification"
-    case .GetCurrentUserFollowing: return "users/me/following"
-    case .FollowUser(let userID): return "users/\(userID)/follow"
-    case .UnfollowUser(let userID): return "users/\(userID)/follow"
-    case .FindUsersByPhoneNumbers: return "users/find_by_contacts"
-    case .GetReelStream: return "reels/stream"
-    case .CreateReel: return "reels"
-    case .UpdateReelTitle(let reelID, _): return "reels/\(reelID)"
-    case .GetReelParticipants(let reelID): return "reels/\(reelID)/participants"
-    case .AddParticipantToReel(let reelID, let userID): return "reels/\(reelID)/participants/\(userID)"
-    case .RemoveCurrentUserAsParticipantInReel(let reelID): return "reels/\(reelID)/participants/me"
-    case .GetUnwatchedClipsInReel(let reelID, _): return "reels/\(reelID)/clips"
-    case .CreateClipInReel(let reelID, _): return "reels/\(reelID)/clips"
-      }
+      case .PhoneAuthentication: return "users/sign_up"
+      case .PhoneVerification: return "users/sign_in"
+      case .GetCurrentUser: return "users/me"
+      case .UpdateCurrentUser: return "users/me"
+      case .GetCurrentUserFollowing: return "users/me/following"
+      case .FollowUser(let userID): return "users/\(userID)/follow"
+      case .UnfollowUser(let userID): return "users/\(userID)/follow"
+      case .FindUsersByPhoneNumbers: return "users"
+      case .FindUsersByUsername: return "users"
+      case .GetClipFeed: return "clips/feed"
+      case .CreateClip: return "clips"
+      case .DeleteClip(let clipID): return "clips/\(clipID)"
+      case .FlagClip(let clipID): return "clips/\(clipID)"
+    }
   }
 
   var parameterEncoding: ParameterEncoding? {
     switch self {
-    case .SignUp: return ParameterEncoding.JSON
-    case .SignIn: return ParameterEncoding.JSON
-    case .UpdateCurrentUser: return ParameterEncoding.JSON
-    case .ChangePhoneNumber: return ParameterEncoding.JSON
-    case .VerifyPhoneNumber: return ParameterEncoding.JSON
-    case .FindUsersByPhoneNumbers: return ParameterEncoding.JSON
-    case .CreateReel: return ParameterEncoding.JSON
-    case .UpdateReelTitle: return ParameterEncoding.JSON
-    case .GetUnwatchedClipsInReel: return ParameterEncoding.URL
-    case .CreateClipInReel: return ParameterEncoding.JSON
-    default: return nil
-      }
+      case .PhoneAuthentication: return ParameterEncoding.JSON
+      case .PhoneVerification: return ParameterEncoding.JSON
+      case .UpdateCurrentUser: return ParameterEncoding.JSON
+      case .FindUsersByPhoneNumbers: return ParameterEncoding.URL
+      case .FindUsersByUsername: return ParameterEncoding.URL
+      case .CreateClip: return ParameterEncoding.JSON
+      default: return nil
+    }
   }
 
   var parameters: [String: AnyObject]? {
     switch self {
-    case .SignUp(let username, let email, let password): return ["user": ["username": username, "email": email, "password": password]]
-    case .SignIn(let email, let password): return ["user": ["email": email, "password": password]]
-    case .UpdateCurrentUser(let username, let email, let name):
-      var userParameters = [String: String]()
-      if let newUsername = username {
-        userParameters["username"] = newUsername
-      }
-      if let newEmail = email {
-        userParameters["email"] = newEmail
-      }
-      if let newName = name {
-        userParameters["name"] = newName
-      }
-      return ["user": userParameters]
-    case .ChangePhoneNumber(let phoneNumber): return ["user": ["phone_number": phoneNumber]]
-    case .VerifyPhoneNumber(let phoneNumberVerificationCode): return ["user": ["phone_number_verification_code": phoneNumberVerificationCode]]
-    case .FindUsersByPhoneNumbers(let phoneNumbers):
-      var contacts = [AnyObject]()
-      for phoneNumber in phoneNumbers as [String] {
-        contacts.append(["phone_number": phoneNumber])
-      }
-      return ["contacts": contacts]
-    case .CreateReel(let title, let participantIDs):
-      var reelParameters = [String: AnyObject]()
-      if let newTitle = title {
-        reelParameters["title"] = newTitle
-      }
-      if let newParticipantIDs = participantIDs {
-        reelParameters["participant_ids"] = newParticipantIDs
-      }
-      return ["reel": reelParameters]
-    case .UpdateReelTitle(let reelID, let title): return ["reel": ["title": title]]
-    case .GetUnwatchedClipsInReel(let reelID, let since):
-      var parameters = [String: AnyObject]()
-      if let sinceDate = since {
-        parameters["since"] = Int(sinceDate.timeIntervalSince1970)
-      }
-      return parameters
-    case .CreateClipInReel(let reelID, let videoData): return ["clip": ["video": NSString(data: videoData, encoding: NSUTF8StringEncoding)!]]
-    default: return nil
-      }
+      case .PhoneAuthentication(let phoneNumber): return ["phone_number": phoneNumber]
+      case .PhoneVerification(_, let phoneNumberVerificationCode): return ["phone_number_verification_code": phoneNumberVerificationCode]
+      case .UpdateCurrentUser(let username, let email, let name):
+        var userParameters = [String: String]()
+        if let newUsername = username {
+          userParameters["username"] = newUsername
+        }
+        if let newEmail = email {
+          userParameters["email"] = newEmail
+        }
+        if let newName = name {
+          userParameters["name"] = newName
+        }
+        return userParameters
+      case .FindUsersByPhoneNumbers(let phoneNumbers): return ["phone_number": ", ".join(phoneNumbers)]
+      case .FindUsersByUsername(let username): return ["username": username]
+      case .CreateClip(let videoData): return ["video": NSString(data: videoData, encoding: NSUTF8StringEncoding)!]
+      default: return nil
+    }
   }
 
   // MARK: URLRequestConvertible
