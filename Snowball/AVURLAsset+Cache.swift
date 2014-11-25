4c89773116061441bf9b0fa37689a5d05fa2662e
James Martinez <jamescmartinez@gmail.com>
2014-11-25 14:25:42 -0800

    fixed video URL cache

--- Snowball/AVURLAsset+Cache.swift
+++ Snowball/AVURLAsset+Cache.swift
@@ -6,23 +6,41 @@
 //  Copyright (c) 2014 Snowball, Inc. All rights reserved.
 //
 
+import Alamofire
 import AVFoundation
 import Foundation
-import Haneke
 
 extension AVURLAsset {
   typealias CompletionHandler = (AVURLAsset?, NSError?) -> ()
 
   class func createAssetFromRemoteURL(URL: NSURL, completionHandler: CompletionHandler? = nil) {
-    let cache = Haneke.sharedDataCache
-    cache.fetch(URL: URL).onSuccess { (_) in
-      // Hacky way of getting cache URL from Haneke
-      let basePath = DiskCache.basePath().stringByAppendingPathComponent("shared-data/original/")
-      let path = DiskCache(path: basePath, capacity: UINT64_MAX).pathForKey(URL.absoluteString!)
-      let cacheURL = NSURL(fileURLWithPath: path)
-      if let completion = completionHandler { completion(AVURLAsset(URL: cacheURL, options: nil), nil) }
-      }.onFailure { (error) in
+    // Create cache file URL using remote URL as key
+    var cacheURL = NSFileManager.defaultManager().URLsForDirectory(.CachesDirectory, inDomains: .UserDomainMask)[0] as? NSURL
+    let key = URL.absoluteString!.stringByReplacingOccurrencesOfString("/", withString: "").stringByReplacingOccurrencesOfString(":", withString: "")
+    cacheURL = cacheURL!.URLByAppendingPathComponent(key)
+
+    // Return asset immediately if it exists in the cache
+    if NSFileManager.defaultManager().fileExistsAtPath(cacheURL!.path!) {
+      if let completion = completionHandler {
+        completion(AVURLAsset(URL: cacheURL!, options: nil), nil)
+        return
+      }
+    }
+
+    // Asset doesn't exist in cache, fetch it
+    Alamofire.download(.GET, URL.absoluteString!) { (temporaryURL, response) in
+      // Specify where to save download (to the cache URL created above)
+      if let cacheURL = cacheURL {
+        return cacheURL
+      }
+      cacheURL = temporaryURL
+      return cacheURL!
+    }.response { (_, response, _, error) in
+      if let error = error {
         if let completion = completionHandler { completion(nil, error) }
+      } else {
+        if let completion = completionHandler { completion(AVURLAsset(URL: cacheURL!, options: nil), nil) }
+      }
     }
   }
 }
