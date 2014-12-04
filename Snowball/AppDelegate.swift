3c809f1dba293b38e1512df070a2a25633c1f337
James Martinez <jamescmartinez@gmail.com>
2014-12-04 10:53:30 -0800

    fix for window still showing status bar area

--- Snowball/AppDelegate.swift
+++ Snowball/AppDelegate.swift
@@ -13,21 +13,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   // MARK: - Properties
 
+  var window: UIWindow?
+
   class var initialViewController: UIViewController {
     return HomeViewController()
   }
 
-  var window: UIWindow = {
-    let window = UIWindow(frame: UIScreen.mainScreen().bounds)
-    window.backgroundColor = UIColor.whiteColor()
-    window.rootViewController = AppDelegate.initialViewController
-    window.makeKeyAndVisible()
-    return window
-  }()
+  // MARK: - Private
+
+  // This is not a property since UIScreen.mainScreen().bounds is not set
+  // until application(_:didFinishLaunchingWithOptions:) is called.
+  private func setWindow() {
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    window!.backgroundColor = UIColor.whiteColor()
+    window!.rootViewController = AppDelegate.initialViewController
+    window!.makeKeyAndVisible()
+  }
 
   // MARK: - UIApplicationDelegate
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    setWindow()
     return true
   }
 }
