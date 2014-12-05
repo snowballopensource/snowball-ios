b00347dca8ce9d16ab51583769f8e44166deed7a
James Martinez <jamescmartinez@gmail.com>
2014-12-05 13:07:52 -0800

    basic clip cell layout

--- Snowball/ClipCollectionViewCell.swift
+++ Snowball/ClipCollectionViewCell.swift
@@ -6,9 +6,54 @@
 //  Copyright (c) 2014 Snowball, Inc. All rights reserved.
 //
 
+import Cartography
 import UIKit
 
 class ClipCollectionViewCell: UICollectionViewCell {
+  let clipThumbnailImageView = UIImageView()
+  let userAvatarImageView = UIImageView()
+  let detailLabel = UILabel()
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    clipThumbnailImageView.backgroundColor = UIColor.blackColor()
+    userAvatarImageView.backgroundColor = UIColor.blackColor()
+    detailLabel.textAlignment = NSTextAlignment.Center
+  }
+
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - UIView
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+
+    contentView.addSubview(clipThumbnailImageView)
+    layout(clipThumbnailImageView) { (clipThumbnailImageView) in
+      clipThumbnailImageView.left == clipThumbnailImageView.superview!.left
+      clipThumbnailImageView.top == clipThumbnailImageView.superview!.top
+      clipThumbnailImageView.right == clipThumbnailImageView.superview!.right
+      clipThumbnailImageView.height == clipThumbnailImageView.superview!.width
+    }
+
+    contentView.addSubview(userAvatarImageView)
+    layout(userAvatarImageView, clipThumbnailImageView) { (userAvatarImageView, clipThumbnailImageView) in
+      userAvatarImageView.centerX == userAvatarImageView.superview!.centerX
+      userAvatarImageView.top == clipThumbnailImageView.bottom + 10
+      userAvatarImageView.width == 40
+      userAvatarImageView.height == userAvatarImageView.width
+    }
+
+    contentView.addSubview(detailLabel)
+    layout(detailLabel, userAvatarImageView) { (detailLabel, userAvatarImageView) in
+      detailLabel.left == detailLabel.superview!.left
+      detailLabel.top == userAvatarImageView.bottom + 5
+      detailLabel.right == detailLabel.superview!.right
+      // detailLabel.height == detailLabel.superview!.width
+    }
+  }
 
   // MARK: - UICollectionReuseableView+Required
 
@@ -19,5 +64,7 @@ class ClipCollectionViewCell: UICollectionViewCell {
     return CGSizeMake(140.0, cellHeight)
   }
 
-  override func configureForObject(object: AnyObject) {}
+  override func configureForObject(object: AnyObject) {
+    detailLabel.text = "Name, 1h"
+  }
 }
