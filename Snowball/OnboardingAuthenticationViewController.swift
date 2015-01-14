0b88147844bcf37c4b7868cd2e8659f3de332146
James Martinez <jamescmartinez@gmail.com>
2015-01-13 18:28:33 -0800

    onboarding sign in

---
+++ Snowball/OnboardingAuthenticationViewController.swift
@@ -0,0 +1,139 @@
+//
+//  OnboardingAuthenticationViewController.swift
+//  Snowball
+//
+//  Created by James Martinez on 1/13/15.
+//  Copyright (c) 2015 Snowball, Inc. All rights reserved.
+//
+
+import Cartography
+import UIKit
+
+class OnboardingAuthenticationViewController: UIViewController, OnboardingTopViewDelegate {
+  let topBar = OnboardingTopView()
+  let messageLabel = UILabel()
+  let usernameTextField = UITextField()
+  let emailTextField = UITextField()
+  let passwordTextField = UITextField()
+  var showUsernameTextField = false
+
+  // MARK: - UIViewController
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = UIColor.whiteColor()
+
+    topBar.setPage(0)
+    view.addSubview(topBar)
+    layout(topBar) { (topBar) in
+      topBar.left == topBar.superview!.left
+      topBar.top == topBar.superview!.top
+      topBar.right == topBar.superview!.right
+      topBar.height == 65
+    }
+
+    let sideMargin: Float = 25
+
+    messageLabel.font = UIFont(name: UIFont.SnowballFont.regular, size: 24)
+    messageLabel.numberOfLines = 2
+    view.addSubview(messageLabel)
+    layout(messageLabel) { (messageLabel) in
+      messageLabel.left == messageLabel.superview!.left + sideMargin
+      messageLabel.top == messageLabel.superview!.top + 65
+      messageLabel.right == messageLabel.superview!.right - sideMargin
+    }
+
+    let betweenMargin: Float = 15
+    let textFieldHeight: Float = 50
+
+    if showUsernameTextField {
+      usernameTextField.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("username"), attributes: [NSForegroundColorAttributeName: UIColor.SnowballColor.greenColor])
+      usernameTextField.textColor = UIColor.SnowballColor.greenColor
+      usernameTextField.tintColor = UIColor.SnowballColor.greenColor
+      usernameTextField.font = UIFont(name: UIFont.SnowballFont.regular, size: 24)
+      usernameTextField.alignLeft()
+      usernameTextField.showSnowballStyleBorderWithColor(UIColor.SnowballColor.greenColor)
+      usernameTextField.autocorrectionType = UITextAutocorrectionType.No
+      usernameTextField.autocapitalizationType = UITextAutocapitalizationType.None
+      view.addSubview(usernameTextField)
+      layout(usernameTextField, messageLabel) { (usernameTextField, messageLabel) in
+        usernameTextField.left == messageLabel.left
+        usernameTextField.top == usernameTextField.superview!.top + 140
+        usernameTextField.right == messageLabel.right
+        usernameTextField.height == textFieldHeight
+      }
+    }
+
+    emailTextField.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("email"), attributes: [NSForegroundColorAttributeName: UIColor.SnowballColor.greenColor])
+    emailTextField.textColor = UIColor.SnowballColor.greenColor
+    emailTextField.tintColor = UIColor.SnowballColor.greenColor
+    emailTextField.font = UIFont(name: UIFont.SnowballFont.regular, size: 24)
+    emailTextField.alignLeft()
+    emailTextField.showSnowballStyleBorderWithColor(UIColor.SnowballColor.greenColor)
+    emailTextField.autocorrectionType = UITextAutocorrectionType.No
+    emailTextField.autocapitalizationType = UITextAutocapitalizationType.None
+    emailTextField.keyboardType = UIKeyboardType.EmailAddress
+    view.addSubview(emailTextField)
+    if showUsernameTextField {
+      layout(emailTextField, usernameTextField) { (emailTextField, usernameTextField) in
+        emailTextField.left == usernameTextField.left
+        emailTextField.top == usernameTextField.bottom + betweenMargin
+        emailTextField.right == usernameTextField.right
+        emailTextField.height == textFieldHeight
+      }
+    } else {
+      layout(emailTextField, messageLabel) { (emailTextField, messageLabel) in
+        emailTextField.left == messageLabel.left
+        emailTextField.top == emailTextField.superview!.top + 140
+        emailTextField.right == messageLabel.right
+        emailTextField.height == textFieldHeight
+      }
+    }
+
+
+    passwordTextField.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("password"), attributes: [NSForegroundColorAttributeName: UIColor.SnowballColor.greenColor])
+    passwordTextField.textColor = UIColor.SnowballColor.greenColor
+    passwordTextField.tintColor = UIColor.SnowballColor.greenColor
+    passwordTextField.font = UIFont(name: UIFont.SnowballFont.regular, size: 24)
+    passwordTextField.alignLeft()
+    passwordTextField.showSnowballStyleBorderWithColor(UIColor.SnowballColor.greenColor)
+    passwordTextField.autocorrectionType = UITextAutocorrectionType.No
+    passwordTextField.autocapitalizationType = UITextAutocapitalizationType.None
+    passwordTextField.secureTextEntry = true
+    view.addSubview(passwordTextField)
+    layout(passwordTextField, emailTextField) { (passwordTextField, emailTextField) in
+      passwordTextField.left == emailTextField.left
+      passwordTextField.top == emailTextField.bottom + betweenMargin
+      passwordTextField.right == emailTextField.right
+      passwordTextField.height == textFieldHeight
+    }
+  }
+
+
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    if showUsernameTextField {
+      usernameTextField.becomeFirstResponder()
+    } else {
+      emailTextField.becomeFirstResponder()
+    }
+  }
+
+  // MARK: - OnboardingTopViewDelegate
+
+  func onboardingTopViewBackButtonTapped() {
+    navigationController?.popViewControllerAnimated(true)
+  }
+
+  func onboardingTopViewForwardButtonTapped() {
+    goForward()
+  }
+
+  // MARK: - OnboardingAuthenticationViewController
+
+  func goForward() {
+    assert(false, "Implement `goForward` in subclass of OnboardingAuthenticationViewController")
+  }
+
+}
--- Snowball/OnboardingSignInViewController.swift
+++ Snowball/OnboardingSignInViewController.swift
@@ -8,5 +8,25 @@
 
 import UIKit
 
-class OnboardingSignInViewController: UIViewController {
+class OnboardingSignInViewController: OnboardingAuthenticationViewController {
+
+  // MARK: - UIViewController
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let messageStringOne = NSAttributedString(string: "Ok, let's get you ", attributes: [NSForegroundColorAttributeName: UIColor.blackColor()])
+    let messageStringTwo = NSAttributedString(string: "back into\n", attributes: [NSForegroundColorAttributeName: UIColor.SnowballColor.greenColor])
+    let messageStringThree = NSAttributedString(string: "your account.", attributes: [NSForegroundColorAttributeName: UIColor.SnowballColor.grayColor])
+    let messageString = NSMutableAttributedString(attributedString: messageStringOne)
+    messageString.appendAttributedString(messageStringTwo)
+    messageString.appendAttributedString(messageStringThree)
+    messageLabel.attributedText = messageString
+  }
+
+  // MARK: - OnboardingAuthenticationViewController
+
+  override func goForward() {
+    println("sign in")
+  }
 }
