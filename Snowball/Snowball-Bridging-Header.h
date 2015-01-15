c4f4ea132392f3a14af4521970ba377e0be8c375
James Martinez <jamescmartinez@gmail.com>
2015-01-15 15:48:43 -0800

    AFNetworking

---
+++ Snowball/Snowball-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Snowball-Bridging-Header.h
+//  Snowball
+//
+//  Created by James Martinez on 1/15/15.
+//  Copyright (c) 2015 Snowball, Inc. All rights reserved.
+//
+
+#import "AFNetworking.h"
