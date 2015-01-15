f11118c9ce2f65c824bee9ec5623f46a3a6d4883
James Martinez <jamescmartinez@gmail.com>
2015-01-14 16:46:39 -0800

    better window code at app launch

--- Snowball/AppDelegate.swift
+++ Snowball/AppDelegate.swift
@@ -23,19 +23,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return HomeViewController()
   }
 
-  // This is not a property since UIScreen.mainScreen().bounds is not set
-  // until application(_:didFinishLaunchingWithOptions:) is called.
-  private func setWindow() {
+  private func setupWindow() {
     window = UIWindow(frame: UIScreen.mainScreen().bounds)
-    window!.backgroundColor = UIColor.whiteColor()
-    window!.rootViewController = AppDelegate.initialViewController
-    window!.makeKeyAndVisible()
+    window?.backgroundColor = UIColor.whiteColor()
+    window?.rootViewController = AppDelegate.initialViewController
+    window?.makeKeyAndVisible()
   }
 
   // MARK: - UIApplicationDelegate
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    setWindow()
+    setupWindow()
     return true
   }
 }
