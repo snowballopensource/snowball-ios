f5dcae9d761130367a98ecbe3e4b2c1f9b00ed41
James Martinez <jamescmartinez@gmail.com>
2015-02-04 21:48:23 -0800

    New followers/following list in progress

--- Snowball/FriendsViewController.swift
+++ Snowball/FriendsViewController.swift
@@ -2,19 +2,69 @@
 //  FriendsViewController.swift
 //  Snowball
 //
-//  Created by James Martinez on 1/26/15.
+//  Created by James Martinez on 2/4/15.
 //  Copyright (c) 2015 Snowball, Inc. All rights reserved.
 //
 
 import Cartography
 import UIKit
 
-class FriendsViewController: UIViewController, SnowballTopViewDelegate, UITableViewDelegate {
+class FriendsViewController: UIViewController {
+
+  // MARK: - Properties
+
   let topView = SnowballTopView(leftButtonType: SnowballTopViewButtonType.Camera, rightButtonType: SnowballTopViewButtonType.AddFriends, title: "Friends")
-  let tableView = UITableView()
-  lazy var tableViewDataSource: FriendsDataSource = {
-    return FriendsDataSource(tableView: self.tableView)
+
+  let currentUserAvatarImageView: UserAvatarImageView = {
+    let currentUserAvatarImageView = UserAvatarImageView()
+    currentUserAvatarImageView.backgroundColor = User.currentUser?.color as? UIColor
+    return currentUserAvatarImageView
   }()
+
+  let currentUserUsernameLabel: UILabel = {
+    let currentUserUsernameLabel = UILabel()
+    currentUserUsernameLabel.textColor = User.currentUser?.color as? UIColor
+    currentUserUsernameLabel.font = UIFont(name: UIFont.SnowballFont.regular, size: 26)
+    currentUserUsernameLabel.text = User.currentUser?.username
+    return currentUserUsernameLabel
+  }()
+
+  let settingsButton: UIButton = {
+    let settingsButton = UIButton()
+    settingsButton.setImage(UIImage(named: "settings"), forState: UIControlState.Normal)
+    return settingsButton
+  }()
+
+  enum FollowersFollowingSegmentedControlIndex: Int {
+    case Following
+    case Followers
+  }
+
+  let followersFollowingSegmentedControl: UISegmentedControl = {
+    let followersFollowingSegmentedControl = UISegmentedControl()
+    let segmentedControlFont = UIFont(name: UIFont.SnowballFont.regular, size: 20)!
+    followersFollowingSegmentedControl.setTitleTextAttributes([NSFontAttributeName: segmentedControlFont], forState: UIControlState.Normal)
+    followersFollowingSegmentedControl.insertSegmentWithTitle(NSLocalizedString("Following"), atIndex: FollowersFollowingSegmentedControlIndex.Following.rawValue, animated: false)
+    followersFollowingSegmentedControl.insertSegmentWithTitle(NSLocalizedString("Followers"), atIndex: FollowersFollowingSegmentedControlIndex.Followers.rawValue, animated: false)
+    followersFollowingSegmentedControl.selectedSegmentIndex = 0
+    followersFollowingSegmentedControl.tintColor = UIColor.blackColor()
+    return followersFollowingSegmentedControl
+  }()
+
+  let tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+    // TODO: change height to a class variable
+    tableView.rowHeight = UserTableViewCell.height()
+    // TODO: use .identifier instead of NSStringFromClass (use search to find all)
+    tableView.registerClass(UserTableViewCell.self, forCellReuseIdentifier: NSStringFromClass(UserTableViewCell))
+    tableView.registerClass(SnowballTableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: NSStringFromClass(SnowballTableViewHeaderFooterView))
+    return tableView
+  }()
+
+  var users: [User] = []
+
+  // MARK: - UIViewController
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,47 +72,93 @@ class FriendsViewController: UIViewController, SnowballTopViewDelegate, UITableV
     view.addSubview(topView)
     topView.setupDefaultLayout()
 
-    tableView.separatorStyle = UITableViewCellSeparatorStyle.None
-    tableView.dataSource = tableViewDataSource
-    tableView.delegate = self
-    tableView.registerHeaderFooterClass(SnowballTableViewHeaderFooterView)
+    let margin: Float = 20
+
+
+    view.addSubview(currentUserAvatarImageView)
+    layout(currentUserAvatarImageView, topView) { (currentUserAvatarImageView, topView) in
+      currentUserAvatarImageView.left == currentUserAvatarImageView.superview!.left + margin
+      currentUserAvatarImageView.top == topView.bottom + 20
+      currentUserAvatarImageView.width == 40
+      currentUserAvatarImageView.height == currentUserAvatarImageView.width
+    }
+
+    view.addSubview(currentUserUsernameLabel)
+    layout(currentUserUsernameLabel, currentUserAvatarImageView) { (currentUserUsernameLabel, currentUserAvatarImageView) in
+      currentUserUsernameLabel.left == currentUserAvatarImageView.right + 15
+      currentUserUsernameLabel.centerY == currentUserAvatarImageView.centerY
+    }
+
+    settingsButton.addTarget(self, action: "settingsButtonTapped", forControlEvents: UIControlEvents.TouchUpInside)
+    view.addSubview(settingsButton)
+    layout(settingsButton, currentUserAvatarImageView) { (settingsButton, currentUserAvatarImageView) in
+      settingsButton.right == settingsButton.superview!.right - margin
+      settingsButton.centerY == currentUserAvatarImageView.centerY
+      settingsButton.width == 44
+      settingsButton.height == settingsButton.width
+    }
+
+    followersFollowingSegmentedControl.addTarget(self, action: "followersFollowingSegmentedControlTapped", forControlEvents: UIControlEvents.ValueChanged)
+    view.addSubview(followersFollowingSegmentedControl)
+    layout(followersFollowingSegmentedControl, currentUserAvatarImageView) { (followersFollowingSegmentedControl, currentUserAvatarImageView) in
+      followersFollowingSegmentedControl.top == currentUserAvatarImageView.bottom + 20
+      followersFollowingSegmentedControl.left == followersFollowingSegmentedControl.superview!.left + margin
+      followersFollowingSegmentedControl.right == followersFollowingSegmentedControl.superview!.right - margin
+      followersFollowingSegmentedControl.height == 40
+    }
+
+    tableView.dataSource = self
     view.addSubview(tableView)
-    layout(tableView, topView) { (tableView, topView) in
+    layout(tableView, followersFollowingSegmentedControl) { (tableView, followersFollowingSegmentedControl) in
       tableView.left == tableView.superview!.left
-      tableView.top == topView.bottom
+      tableView.top == followersFollowingSegmentedControl.bottom + 15
       tableView.right == tableView.superview!.right
       tableView.bottom == tableView.superview!.bottom
     }
+
+    refresh()
   }
 
-  // MARK: - UITableViewDelegate
+  // MARK: - Actions
 
-  // TODO: there is probably a better way to do these delegate methods akin to the way we're handling data source
-  func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-    return UserTableViewCell.height()
+  func settingsButtonTapped() {
+    navigationController?.pushViewController(EditProfileViewController(), animated: true)
   }
 
-  func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-    let headerView = SnowballTableViewHeaderFooterView()
-    if section == FriendsTableViewSection.Me.rawValue {
-      headerView.titleLabel.text = NSLocalizedString("ME")
-    } else {
-      headerView.titleLabel.text = NSLocalizedString("MY FRIENDS")
+  func followersFollowingSegmentedControlTapped() {
+    refresh()
+  }
+
+  // MARK: - Private
+
+  private func refresh() {
+    // TODO: remove duplication
+    switch(followersFollowingSegmentedControl.selectedSegmentIndex) {
+    case FollowersFollowingSegmentedControlIndex.Following.rawValue:
+      API.request(Router.GetCurrentUserFollowing).responseJSON { (request, response, JSON, error) in
+        error?.print("api get current user following/followers")
+        if let JSON: AnyObject = JSON {
+          self.users = User.objectsFromJSON(JSON) as [User]
+          self.tableView.reloadData()
+        }
+      }
+    case FollowersFollowingSegmentedControlIndex.Followers.rawValue:
+      // TODO: get followers
+      API.request(Router.GetCurrentUserFollowing).responseJSON { (request, response, JSON, error) in
+        error?.print("api get current user following/followers")
+        if let JSON: AnyObject = JSON {
+          self.users = User.objectsFromJSON(JSON) as [User]
+          self.tableView.reloadData()
+        }
+      }
+    default: return
     }
-    return headerView
   }
+}
 
-  func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-    return SnowballTableViewHeaderFooterView.height()
-  }
+// MARK: -
 
-  func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-    return UIView()
-  }
-
-  func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-    return 20
-  }
+extension FriendsViewController: SnowballTopViewDelegate {
 
   // MARK: - SnowballTopViewDelegate
 
@@ -72,63 +168,29 @@ class FriendsViewController: UIViewController, SnowballTopViewDelegate, UITableV
 
   func snowballTopViewRightButtonTapped() {
     // TODO: go to add friends
-    println("go to add friends")
-  }
-
-  // MARK: - CurrentUserTableViewCellDelegate
-
-  func settingsButtonTapped() {
-    navigationController?.pushViewController(EditProfileViewController(), animated: true)
   }
 }
 
 // MARK: -
 
-enum FriendsTableViewSection: Int {
-  case Me
-  case Friends
-}
+extension FriendsViewController: UITableViewDataSource {
 
-// MARK: -
+  // MARK: - UITableViewDataSource
 
-class FriendsDataSource: FetchedResultsTableViewDataSource {
-  var currentUserCellDelegate: CurrentUserTableViewCellDelegate?
-
-  init(tableView: UITableView) {
-    let cellTypes = [CurrentUserTableViewCell.self, UserTableViewCell.self] as [UITableViewCell.Type]
-    let currentUserID = User.currentUser!.id ?? ""
-    super.init(tableView: tableView, entityName: User.entityName(), sortDescriptors: [NSSortDescriptor(key: "username", ascending: true)], predicate: NSPredicate(format: "id != %@", currentUserID), cellTypes: cellTypes)
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return users.count
   }
 
-  // MARK: - TableViewDataSource
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(UserTableViewCell),
+      forIndexPath: indexPath) as UITableViewCell
+    configureCell(cell, atIndexPath: indexPath)
+    return cell
+  }
 
-  override func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+  func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
     let cell = cell as UserTableViewCell
-    if indexPath.section == FriendsTableViewSection.Me.rawValue {
-      let currentUserCell = cell as CurrentUserTableViewCell
-      currentUserCell.delegate = currentUserCellDelegate
-      if let user = User.currentUser {
-        cell.configureForObject(user)
-      }
-    } else {
-      super.configureCell(cell, atIndexPath: originalIndexPathFromMappedIndexPath(indexPath))
-    }
-  }
-
-  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 2
-  }
-
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    switch section {
-    case FriendsTableViewSection.Me.rawValue: return 1
-    default: return super.tableView(tableView, numberOfRowsInSection: 0)
-    }
-  }
-
-  // MARK: - Index Path Mapping
-
-  func originalIndexPathFromMappedIndexPath(indexPath: NSIndexPath) -> NSIndexPath {
-    return NSIndexPath(forRow: indexPath.row, inSection: 0)
+    let user = users[indexPath.row]
+    cell.configureForObject(user)
   }
 }
