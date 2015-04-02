69983e7dd0da97f4132db265edd9d90187e2c965
James Martinez <jamescmartinez@gmail.com>
2015-04-02 15:00:23 -0700

    Fix for deleting clip while uploading

--- Snowball/ClipsViewController.swift
+++ Snowball/ClipsViewController.swift
@@ -316,22 +316,24 @@ extension ClipsViewController: ClipCollectionViewCellDelegate {
 
   func userDidDeleteClipForCell(cell: ClipCollectionViewCell) {
     if let clip = clipForCell(cell) {
-      if let id = clip.id {
-        let alertController = UIAlertController(title: NSLocalizedString("Delete this clip?"), message: NSLocalizedString("Are you sure you want to delete this clip?"), preferredStyle: UIAlertControllerStyle.Alert)
-        alertController.addAction(UIAlertAction(title: NSLocalizedString("Don't Delete"), style: UIAlertActionStyle.Cancel, handler: nil))
-        alertController.addAction(UIAlertAction(title: NSLocalizedString("Delete"), style: UIAlertActionStyle.Destructive) { (action) in
-          let index = self.indexOfClip(clip)
-          self.clips.removeAtIndex(index)
-          self.collectionView.deleteItemsAtIndexPaths([NSIndexPath(forItem: index, inSection: 0)])
+      let alertController = UIAlertController(title: NSLocalizedString("Delete this clip?"), message: NSLocalizedString("Are you sure you want to delete this clip?"), preferredStyle: UIAlertControllerStyle.Alert)
+      alertController.addAction(UIAlertAction(title: NSLocalizedString("Don't Delete"), style: UIAlertActionStyle.Cancel, handler: nil))
+      alertController.addAction(UIAlertAction(title: NSLocalizedString("Delete"), style: UIAlertActionStyle.Destructive) { (action) in
+        let index = self.indexOfClip(clip)
+        self.clips.removeAtIndex(index)
+        self.collectionView.deleteItemsAtIndexPaths([NSIndexPath(forItem: index, inSection: 0)])
+        if let id = clip.id {
           API.request(Router.DeleteClip(clipID: id)).response { (request, response, JSON, error) in
             if error != nil { displayAPIErrorToUser(JSON); return }
-            // TODO: Remove clip from timeline in here (three lines above API)
+            // TODO: Remove clip from timeline in here
             // This should be done AFTER the new upload style is completed, so we have an ID to delete immediately.
+            // let index = self.indexOfClip(clip)
+            // self.clips.removeAtIndex(index)
+            // self.collectionView.deleteItemsAtIndexPaths([NSIndexPath(forItem: index, inSection: 0)])
           }
-          return
-          })
-        presentViewController(alertController, animated: true, completion: nil)
-      }
+        }
+        })
+      presentViewController(alertController, animated: true, completion: nil)
     }
   }
 }
