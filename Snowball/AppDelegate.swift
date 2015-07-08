4d356ae6cb18568d25366eba98af4c8338ada78d
James Martinez <jamescmartinez@gmail.com>
2015-07-08 14:35:06 -0700

    Shake to get new color scheme

--- Snowball/AppDelegate.swift
+++ Snowball/AppDelegate.swift
@@ -37,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window?.makeKeyAndVisible()
     Fabric.with([Crashlytics()])
     PushManager.registerForPushNotifications()
+    application.applicationSupportsShakeToEdit = true
     return true
   }
 
