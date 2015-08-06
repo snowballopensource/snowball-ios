c5c2fbd23ff10f1e53351ff15d1701b224249c60
James Martinez <jamescmartinez@gmail.com>
2015-08-06 15:30:36 -0700

    Show confirmation when deleting a clip

--- Snowball/TimelineViewController.swift
+++ Snowball/TimelineViewController.swift
@@ -201,15 +201,21 @@ extension TimelineViewController: ClipCollectionViewCellDelegate {
   func userDidTapDeleteButtonForCell(cell: ClipCollectionViewCell) {
     let clip = clipForCell(cell)
     if clip?.user == User.currentUser, let clipID = clip?.id, let clip = clip {
-      // TODO: Show spinner
-      API.request(Router.DeleteClip(clipID: clipID)).responseJSON { (request, response, JSON, error) in
-        if let error = error {
-          println(error)
-          // TODO: Display the error
-        } else {
-          self.timeline.deleteClip(clip)
+      let alertController = UIAlertController(title: NSLocalizedString("Delete this clip?", comment: ""), message: NSLocalizedString("Are you sure you want to delete this clip?", comment: ""), preferredStyle: UIAlertControllerStyle.ActionSheet)
+      alertController.addAction(UIAlertAction(title: NSLocalizedString("Don't Delete", comment: ""), style: UIAlertActionStyle.Cancel, handler: nil))
+      let deleteAction = UIAlertAction(title: NSLocalizedString("Delete", comment: ""), style: UIAlertActionStyle.Destructive) { (action) in
+        // TODO: Show spinner
+        API.request(Router.DeleteClip(clipID: clipID)).responseJSON { (request, response, JSON, error) in
+          if let error = error {
+            println(error)
+            // TODO: Display the error
+          } else {
+            self.timeline.deleteClip(clip)
+          }
         }
       }
+      alertController.addAction(deleteAction)
+      presentViewController(alertController, animated: true, completion: nil)
     }
   }
 
