faf554cead11c73d8d8b2e1b5ede8bf818a55481
James Martinez <jamescmartinez@gmail.com>
2015-08-10 12:31:20 -0700

    Remove the shake detection to change color scheme

--- Snowball/UIViewController+Shake.swift
+++ Snowball/UIViewController+Shake.swift
@@ -12,20 +12,20 @@ import UIKit
 extension UIViewController {
   override public func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
     if motion == UIEventSubtype.MotionShake {
-      let alertController = UIAlertController(title: NSLocalizedString("Shaking it up!", comment: ""), message: "New color scheme, coming right up!", preferredStyle: UIAlertControllerStyle.Alert)
-      alertController.addAction(UIAlertAction(title: NSLocalizedString("Cool!", comment: ""), style: UIAlertActionStyle.Cancel, handler: nil))
-
-      if let rootVC = AppDelegate.sharedDelegate.window!.rootViewController {
-        rootVC.presentViewController(alertController, animated: true) {
-          let users = User.findAll() as! [User]
-          for user in users {
-            user.color = UIColor.SnowballColor.randomColor
-          }
-          if let context = users.first!.managedObjectContext {
-            context.save(nil)
-          }
-        }
-      }
+//      let alertController = UIAlertController(title: NSLocalizedString("Shaking it up!", comment: ""), message: "New color scheme, coming right up!", preferredStyle: UIAlertControllerStyle.Alert)
+//      alertController.addAction(UIAlertAction(title: NSLocalizedString("Cool!", comment: ""), style: UIAlertActionStyle.Cancel, handler: nil))
+//
+//      if let rootVC = AppDelegate.sharedDelegate.window!.rootViewController {
+//        rootVC.presentViewController(alertController, animated: true) {
+//          let users = User.findAll() as! [User]
+//          for user in users {
+//            user.color = UIColor.SnowballColor.randomColor
+//          }
+//          if let context = users.first!.managedObjectContext {
+//            context.save(nil)
+//          }
+//        }
+//      }
     }
   }
 }
