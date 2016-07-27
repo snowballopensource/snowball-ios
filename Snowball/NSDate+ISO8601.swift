4ab935b1ac12bbeca62879a8d7c8592206c8440f
James Martinez <jamescmartinez@gmail.com>
2016-07-26 22:29:38 -0700

    Fix for ISO8601 handling

--- Snowball/NSDate+ISO8601.swift
+++ Snowball/NSDate+ISO8601.swift
@@ -13,7 +13,7 @@ extension NSDate {
     let dateFormatter = NSDateFormatter()
     dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
     dateFormatter.timeZone = NSTimeZone.localTimeZone()
-    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS"
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
     return dateFormatter.dateFromString(ISO8610String)
   }
 }
